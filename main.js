91cbf893b4c8320ae9887886c028a021e49baaf3
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-07-12 23:20:08 +0700

    init npm and express basic package

---
+++ main.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 3000
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("You've hit Santuke");
+})
+app.get('/rand', (req, res) => {
+    // TODO - return random sentences from random language
+});
+app.get('/rand/:country', (req, res) => {
+    const country = req.params.country;
+    // TODO - return random sentences from selected language
+});
+
+app.listen(port, () => console.log("Santuke has been summoned :)"))
