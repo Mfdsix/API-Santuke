8734eb6ce522c94673622812087bf68567d88e9f
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-07-13 12:43:35 +0700

    add swagger ui documentation

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
 
 const SentenceController = require('./controllers/sentence');
 const Log = require('./utils/log');
 
+const swaggerDocument = require('./swagger.json');
 const app = express();
 const port = 3000;
 app.use(cors());
@@ -12,6 +14,10 @@ app.get('/', (req, res) => {
     Log.R("/");
     res.send("You've hit Santuke");
 })
+app.use('/api-docs',
+    swaggerUi.serve, 
+    swaggerUi.setup(swaggerDocument)
+);
 app.get('/rand', async (req, res) => {
     Log.R("/rand");
     res.send(await SentenceController.get());
